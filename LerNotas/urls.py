03dc058d9ce474fa0b1756abe390f99ac564cc4c
mikaelbrehmer <mikaelbrec@gmail.com>
2025-09-15 19:49:09 -0300

    fix: Atualiza erro no url

--- LerNotas/urls.py
+++ LerNotas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SubirNota, upload_nota_fiscal
 
 urlpatterns = [
-    path('', SubirNota, name='subir_nota')
+    path('', SubirNota, name='subir_nota'),
     path('upload/', upload_nota_fiscal, name='upload_nota_fiscal'),
 
 ]
